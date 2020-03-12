662e4973995ff7de5ba27f99f60cdd3d991d7d5f
Purushottam10 <purushottamn8@gmail.com>
2020-03-12 18:34:36 +0530

    ProductInfo WebScraper

--- src/main/java.io.dz/product/model/AmazonItem.java
+++ src/main/java.io.dz/product/model/AmazonItem.java
@@ -4,22 +4,68 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AmazonItem  implements Serializable {
-  private String brand;
-  private String vendorUrl;
+  private Integer id;
+  private String brand = StringUtils.EMPTY;
+  private String dealName = StringUtils.EMPTY;
+  private Integer categoryId;
+  private String dealType = StringUtils.EMPTY;
   private Integer vendorId;
-  private BigDecimal sellingPrice;
-  private Integer discount;
-  private String source;
-  private Integer reviews;
-  private BigDecimal basePrice;
+  private String vendorUrl = StringUtils.EMPTY;
+  private String shortUrl = StringUtils.EMPTY;
+  private String images = StringUtils.EMPTY;
+  private String snapshotUrl = StringUtils.EMPTY;
+  private String tags = StringUtils.EMPTY;
+  private Float basePrice = 0f;
+  private Float sellingPrice = 0f;
+  private Integer discount = 0;
+  private Boolean active = true;
+  private Boolean enabled;
+  private Integer reviews = 0;
+  private Integer rating = 0;
+  private String source = StringUtils.EMPTY;
+
+  private String dealkey = StringUtils.EMPTY;
+  private String dealURLHash = StringUtils.EMPTY;
+  private Date createTimestamp;
+  private Date updateTimestamp;
+
+  @Override
+  public String toString() {
+    return "Deal{" +
+            "brand='" + brand + '\'' +
+            ", dealName='" + dealName + '\'' +
+            ", categoryId=" + categoryId +
+            ", dealType='" + dealType + '\'' +
+            ", vendorId=" + vendorId +
+            ", vendorUrl='" + vendorUrl + '\'' +
+            ", shortUrl='" + shortUrl + '\'' +
+            ", images='" + images + '\'' +
+            ", snapshotUrl='" + snapshotUrl + '\'' +
+            ", tags='" + tags + '\'' +
+            ", basePrice=" + basePrice +
+            ", sellingPrice=" + sellingPrice +
+            ", discount=" + discount +
+            ", active=" + active +
+            ", enabled=" + enabled +
+            ", reviews=" + reviews +
+            ", rating=" + rating +
+            ", source='" + source + '\'' +
+            ", dealkey='" + dealkey + '\'' +
+            ", dealURLHash='" + dealURLHash + '\'' +
+            ", createTimestamp=" + createTimestamp +
+            ", updateTimestamp=" + updateTimestamp +
+            '}';
+  }
 
 }
--- src/main/java.io.dz/product/model/ProductInfo.java
+++ src/main/java.io.dz/product/model/ProductInfo.java
@@ -1,41 +1,137 @@
 package product.model;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProductInfo {
-  public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-    WebClient client = new WebClient();
-    client.getOptions().setCssEnabled(false);
-    client.getOptions().setJavaScriptEnabled(false);
-    String productUrl = "https://www.amazon.in/Whirlpool-1-5T-MAGICOOL-PRO-COPR/dp/B07P5HJJW8/ref=gbph_img_m-4_a3bd_43a06e36?smid=AT95IG9ONZD7S&pf_rd_p=ef5bf9a4-b2b7-4f3b-bd94-c71a6be1a3bd&pf_rd_s=merchandised-search-4&pf_rd_t=101&pf_rd_i=3474656031&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_r=VFWDSDPG23GEK2S1PZDQ";
+  public static void main(String[] args) throws FailingHttpStatusCodeException, IOException {
+    ProductInfo productInfo = new ProductInfo();
+    String url = "https://www.amazon.in/s/ref=mega_elec_s23_7_3_1_2?rh=i%3Acomputers%2Cn%3A14095180031&ie=UTF8";
+    productInfo.getProductInfo(url);
+  }
 
-    HtmlPage page = client.getPage(productUrl);
-    HtmlElement productNode = ((HtmlElement) page
-            .getFirstByXPath("//*[@itemtype='https://schema.org/Product']"));
-    URL imageUrl = new URL((((HtmlElement) productNode.getFirstByXPath("./img")))
-            .getAttribute("src"));
-    HtmlElement offers = ((HtmlElement) productNode.getFirstByXPath("./span[@itemprop='offers']"));
+  public List<AmazonItem> getProductInfo(String url) throws FailingHttpStatusCodeException, IOException {
+    Document document = Jsoup.connect(url)
+            .get();
+    Elements products = document.select("ul > li.s-result-item > div.s-item-container");
+    List<AmazonItem> amazonItems = new ArrayList<>();
+    for (Element row : products) {
+      AmazonItem amazonItem = new AmazonItem();
+      amazonItem.setBasePrice(this.getActualPrice(row));
+      amazonItem.setBrand(this.getBrandName(row));
+      amazonItem.setDiscount(this.getSavingPrice(row));
+      amazonItem.setDealName(this.getTitle(row));
+      amazonItem.setShortUrl(this.getImageLocation(row));
+      amazonItem.setRating(this.getRating(row));
+      amazonItem.setReviews(this.getReview(row));
+      amazonItem.setDiscount(this.getSavingPrice(row));
+      amazonItem.setSellingPrice(this.getSellingPrice(row));
+      amazonItem.setVendorUrl(this.getProductUrl(row));
+      amazonItems.add(amazonItem);
+    }
+    System.out.println(amazonItems.size());
+    return amazonItems;
+  }
 
-    BigDecimal price = new BigDecimal(((HtmlElement) offers.getFirstByXPath("./span[@itemprop='price']")).asText());
-    String productName = (((HtmlElement) productNode.getFirstByXPath("./span[@itemprop='name']")).asText());
-    String currency = (((HtmlElement) offers.getFirstByXPath("./*[@itemprop='priceCurrency']")).getAttribute("content"));
-    String productSKU = (((HtmlElement) productNode.getFirstByXPath("./span[@itemprop='sku']")).asText());
+  private String getTitle(Element rowData) {
+    return rowData.select("a.s-access-detail-page > h2").text();
+  }
 
-    AmazonItem product = new AmazonItem();
-    product.setBrand(productName);
-    product.setBasePrice(price);
-    ObjectMapper mapper = new ObjectMapper();
-    String jsonString = mapper.writeValueAsString(product) ;
-    System.out.println(jsonString);
+  private String getImageLocation(Element rowData) {
+    return rowData.select("div.a-column.a-span12.a-text-center > a.a-link-normal.a-text-normal > img[src]").text();
+  }
+
+  private String getProductUrl(Element rowData) {
+    return rowData.select("div.a-row.a-spacing-small > div.a-row.a-spacing-none > a[href]").text();
+  }
+
+  private int getReview(Element rowData) {
+    String noOfReview = rowData.select("div.a-column.a-span5.a-span-last >" +
+            "                div.a-row.a-spacing-mini > " +
+            "                a.a-size-small.a-link-normal.a-text-normal").text();
+    return Integer.parseInt(noOfReview.replaceAll(",", ""));
+  }
+
+  private float getSellingPrice(Element rowData) {
+    String sellingPriceSelector[] = {"span.a-size-base.a-color-price.s-price.a-text-bold", "span.a-price-whole"};
+    String sellingPrice = "0";
+    for (int i = 0; i < sellingPriceSelector.length - 1; i++) {
+      sellingPrice = rowData.select(sellingPriceSelector[i]).text();
+      if (!sellingPrice.isEmpty())
+        break;
+    }
+    if (sellingPrice.contains("-")) {
+      String temp = sellingPrice.substring(0, sellingPrice.indexOf("-") - 1);
+      return Float.parseFloat(temp.replaceAll(",", ""));
+    } else {
+      return Float.parseFloat(sellingPrice.replaceAll(",", ""));
+    }
+
+  }
+
+  private String getBrandName(Element rowData) {
+    String brandSelector[] = {"div.a-row.a-spacing-small > div.a-row.a-spacing-none > span:nth-child(2)",
+            "div.a-row.a-spacing-small > div.a-row.a-spacing-none"};
+    String brandName = StringUtils.EMPTY;
+    for (int i = 0; i < brandSelector.length - 1; i++) {
+      brandName = rowData.select(brandSelector[i]).text();
+      if (!brandName.isEmpty())
+        break;
+    }
+    return brandName;
+  }
+
+  private float getActualPrice(Element rowData) {
+    String actualPriceSelector[] = {"span.a-size-small.a-color-secondary.a-text-strike", "span.a-price.a-text-price > span.a-offscreen",
+            "span.a-color-price", "div.a-row.a-size-base.a-color-secondary > span.a-color-price"};
+    String actualPrice = "0";
+    Elements actualPriceEl;
+    for (int i = 0; i < actualPriceSelector.length - 1; i++) {
+      actualPriceEl = rowData.select(actualPriceSelector[i]);
+      if (!actualPriceEl.isEmpty()) {
+        actualPrice = actualPriceEl.get(0).text();
+        break;
+      }
+    }
+    if (actualPrice.contains("-")) {
+      String temp = actualPrice.substring(0, actualPrice.indexOf("-") - 1);
+      return Float.parseFloat(temp.replaceAll(",", ""));
+    } else {
+      return Float.parseFloat(actualPrice.replaceAll(",", ""));
+    }
+  }
+
+  private int getSavingPrice(Element rowData) {
+    String savingPriceSelector[] = {"span.a-size-small.a-color-price", "div.a-section.a-spacing-none.a-spacing-top-small > div > div > span:nth-child(3)"};
+    String savingPrice = "0";
+    String subSavingPrice = "";
+    for (int i = 0; i < savingPriceSelector.length - 1; i++) {
+      savingPrice = rowData.select(savingPriceSelector[i]).text();
+      if (!savingPrice.isEmpty()) {
+        subSavingPrice = savingPrice.substring(0, savingPrice.indexOf("(") - 1);
+        break;
+      }
+    }
+    if (subSavingPrice.isEmpty()) {
+      return 0;
+    } else {
+      return Integer.parseInt(subSavingPrice.replaceAll(",", ""));
+    }
+  }
+
+  private int getRating(Element rowData) {
+    String rating = rowData.select("i.a-icon-star > span").text();
+    String subRating = rating.substring(0, rating.indexOf(".") + 2);
+    float ratingVal = Float.parseFloat(subRating);
+    return Math.round(ratingVal);
   }
 }
